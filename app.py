31e24b544c656790a188a40f1636a2f385ea43f3
RedGhost1505 <joshalejandro117@gmail.com>
2024-10-02 10:31:40 -0600

    Open & Close camera

--- app.py
+++ app.py
@@ -2,31 +2,26 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import cv2
 
-
 app = Flask(__name__)
 socketio = SocketIO(app)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-cap = cv2.VideoCapture(0)
+cap = None
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-# @socketio.on('pull_in')
-# def int():
-#     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-#     cap = cv2.VideoCapture(0)
-
-
 @socketio.on('request_frame')
 def send_frame():
-    # face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    # cap = cv2.VideoCapture(0)
-
+    global cap
+    if cap is None or not cap.isOpened():  # Verifica si la cámara no está abierta
+        cap = cv2.VideoCapture(0)  # Abre la cámara de nuevo
 
     while True:
         ret, frame = cap.read()
+        if not ret:
+            break
+
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
 
@@ -39,11 +34,15 @@ def send_frame():
 
         emit('new_frame', {'frame': frame_data}, broadcast=True)
 
+    # No liberes la cámara aquí, ya que seguirás necesitando el loop
     # cap.release()
 
 @socketio.on('pull_out')
 def disconnection():
-    cap.release()
+    global cap
+    if cap is not None and cap.isOpened():
+        cap.release()  # Cierra la cámara cuando sea necesario
+        cap = None  # Resetea la variable cap para indicar que la cámara está cerrada
 
 if __name__ == '__main__':
     socketio.run(app)
